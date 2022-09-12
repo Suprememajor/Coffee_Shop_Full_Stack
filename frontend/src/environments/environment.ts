237a25e19791148f70d1975041ae543ef569272c
Suprememajor <ndipnobertetta@gmail.com>
2022-09-12 19:40:03 +0100

    updated auth0 info

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
+    url: 'dev-g310bp-8.us', // the auth0 domain prefix
+    audience: 'http://www.coffee-shop-api.com', // the audience set for the auth0 app
+    clientId: 'WAdAVFv1JYT24sljyxt9vOFRteHbnIEr', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
   }
 };
